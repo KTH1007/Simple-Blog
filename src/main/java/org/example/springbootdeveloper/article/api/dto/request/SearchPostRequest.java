448a63767864eb8010fdb002f28d0c0f88f92841
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-01-21 01:27:19 +0900

    Revert ":sparkles: Feat: 블로그 글 API"
    This reverts commit 61f8b12d975ae7b89aad6f7e26ee323ff95883df.

--- src/main/java/org/example/springbootdeveloper/article/api/dto/request/SearchPostRequest.java
+++
@@ -1,7 +0,0 @@
-package org.example.springbootdeveloper.article.api.dto.request;
-
-public record SearchPostRequest(
-        String title,
-        String content
-) {
-}
--- src/main/java/org/example/springbootdeveloper/article/domain/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.springbootdeveloper.article.domain.repository;
-
-import org.example.springbootdeveloper.article.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
